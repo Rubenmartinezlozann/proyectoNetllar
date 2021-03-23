9440795fbdfa18071e95d3ba468d4ee669ae1182
ruben <rubenmartinezlozano@gmail.com>
2021-03-23 18:34:52 +0100

    solución provisional para calles duplicadas

--- API/src/Repository/AddressRepository.php
+++ API/src/Repository/AddressRepository.php
@@ -25,7 +25,6 @@ class AddressRepository extends ServiceEntityRepository
 
     public function suggestAddress($addressText)
     {
-        // $addressCombinationsArray = $this->getAddressCombinations($this->getWordsOrderCombinations(), $this->getWordsLenghtSequences($words, $dbAddressData));
         $dbAddressData = $this->findAll();
         $words = $this->getWords($addressText);
         $sequencesArray = $this->getWordsSequences();
@@ -63,7 +62,6 @@ class AddressRepository extends ServiceEntityRepository
                         if ($second !== $third && $third !== $frist) {
                             foreach ($wordsSort as $key => $fourth) {
                                 if ($fourth !== $frist && $fourth !== $second && $fourth !== $third) {
-                                    // $wordsOrder[] = [$frist => null, $second => null, $third => null, $fourth => null];
                                     $wordsOrder[] = [$frist, $second, $third, $fourth];
                                 }
                             }
@@ -75,29 +73,8 @@ class AddressRepository extends ServiceEntityRepository
         return $wordsOrder;
     }
 
-    /* private function getWordsLenghtCombinations($words, $addressData)
-    {
-        $wordsLenght = [];
-        $maxWords = $this->getMaxWordsNumber($addressData, count($words));
-
-        for ($wordsStreet = $maxWords['street']; $wordsStreet >= 0; $wordsStreet--) {
-            for ($wordsProvince = $maxWords['province']; $wordsProvince >= 0; $wordsProvince--) {
-                for ($wordsMunicipality = $maxWords['municipality']; $wordsMunicipality >= 0; $wordsMunicipality--) {
-                    for ($wordsTypeRoad = $maxWords['typeRoad']; $wordsTypeRoad  >= 0; $wordsTypeRoad--) {
-                        $sum = $wordsStreet + $wordsProvince + $wordsMunicipality + $wordsTypeRoad;
-                        if (count($words) === $sum) {
-                            $wordsLenght[] = ['street' => $wordsStreet, 'province' => $wordsProvince, 'municipality' => $wordsMunicipality, 'typeRoad' => $wordsTypeRoad];
-                        }
-                    }
-                }
-            }
-        }
-        return $wordsLenght;
-    } */
-
     private function getWordsLenghtSequences($words, $addressData, $wordsSequences)
     {
-        $wordsLenght = [];
         $maxWords = $this->getMaxWordsNumber($addressData, count($words));
         foreach ($wordsSequences as $wordSequence) {
             for ($words1 = $maxWords[$wordSequence[0]]; $words1 >= 0; $words1--) {
@@ -135,59 +112,6 @@ class AddressRepository extends ServiceEntityRepository
         return $maxWords;
     }
 
-    /* private function getAddressCombinations($wordsOrderArray, $wordsLenghtArray)
-    {
-        foreach ($wordsOrderArray as $orderArray) {
-            foreach ($wordsLenghtArray as $lenghtArray) {
-                $aux = $orderArray;
-                foreach ($aux as $key => $valueOrder) {
-                    $aux[$key] = $lenghtArray[$key];
-                }
-                $combinations[] = $aux;
-            }
-        }
-
-        return $combinations;
-    } */
-
-    /* private function getSuggestedAddress($combinations, $words, $dbAddressData)
-    {
-        $data = [];
-        foreach ($combinations as $combination) {
-            $aux = $combination;
-            $sugestedAddress = [];
-            $index = 0;
-            foreach ($aux as $key => $value) {
-                $text = '';
-                for ($i = $index; $i < $value + $index; $i++) {
-                    $text .= $words[$index];
-                    $text .= $index < ($value + $index - 1) ? ' ' : '';
-                }
-                $index = $i;
-                $aux[$key] = $text;
-                $sugestedAddress[] = $aux;
-            }
-
-            foreach ($sugestedAddress as $sugestedValue) {
-                foreach ($dbAddressData as $dbValue) {
-                    if (str_contains(strtolower($dbValue->getProvincia()), strtolower($sugestedValue['province'])) && str_contains(strtolower($dbValue->getMunicipio()), strtolower($sugestedValue['municipality'])) && str_contains(strtolower($dbValue->getTipovia()), strtolower($sugestedValue['typeRoad'])) && str_contains(strtolower($dbValue->getCalle()), strtolower($sugestedValue['street']))) {
-                        if ($aux !== $dbValue->getCalle()) {
-                            $data[] = [
-                                strtoupper(substr($dbValue->getTipovia(), 0, 1)) . strtolower(substr($dbValue->getTipovia(), 1)),
-                                strtoupper(substr($dbValue->getCalle(), 0, 1)) . strtolower(substr($dbValue->getCalle(), 1)),
-                                strtoupper(substr($dbValue->getMunicipio(), 0, 1)) . strtolower(substr($dbValue->getMunicipio(), 1)),
-                                strtoupper(substr($dbValue->getProvincia(), 0, 1)) . strtolower(substr($dbValue->getProvincia(), 1)),
-                                $dbValue->getNumero()
-                            ];
-                        }
-                    }
-                    $aux = $dbValue->getCalle();
-                }
-            }
-        }
-        return !empty($data) ? $data : [];
-    } */
-
     private function getSuggestedAddress($combinations, $words, $dbAddressData)
     {
         $data = [];
@@ -199,99 +123,37 @@ class AddressRepository extends ServiceEntityRepository
                 for ($i = $index; $i < $value + $index; $i++) {
                     $text .= $words[$i] . ' ';
                 }
-                $text = substr($text, 0, strlen($text)-1);
-                echo ($text . '|');
+                $text = substr($text, 0, strlen($text) - 1);
                 $index = $i;
                 $sugestedValue[$key] = $text !== null ? $text : '';
             }
-            // foreach ($sugestedAddress as $sugestedValue) {
-            $aux = '';
+
+
+            $lastValue = '';
             foreach ($dbAddressData as $dbValue) {
                 if (str_contains(strtolower($dbValue->getProvincia()), strtolower($sugestedValue['province'])) && str_contains(strtolower($dbValue->getMunicipio()), strtolower($sugestedValue['municipality'])) && str_contains(strtolower($dbValue->getTipovia()), strtolower($sugestedValue['typeRoad'])) && str_contains(strtolower($dbValue->getCalle()), strtolower($sugestedValue['street']))) {
-                    if ($aux !== $dbValue->getCalle()) {
+                    if (strtolower($lastValue) !== strtolower($dbValue->getCalle())) {
                         $data[] = [
                             strtoupper(substr($dbValue->getTipovia(), 0, 1)) . strtolower(substr($dbValue->getTipovia(), 1)),
                             strtoupper(substr($dbValue->getCalle(), 0, 1)) . strtolower(substr($dbValue->getCalle(), 1)),
                             strtoupper(substr($dbValue->getMunicipio(), 0, 1)) . strtolower(substr($dbValue->getMunicipio(), 1)),
                             strtoupper(substr($dbValue->getProvincia(), 0, 1)) . strtolower(substr($dbValue->getProvincia(), 1)),
-                            $dbValue->getNumero()
                         ];
                     }
                 }
-                $aux = $dbValue->getCalle();
+                $lastValue = $dbValue->getCalle();
             }
-            // }
+        }
+
+        foreach ($data as $key => $value) {
+            foreach ($data as $key2 => $value2) {
+                if ($key !== $key2 && $value === $value2) {
+                    unset($data[$key]);
+                }
+            }
         }
         return !empty($data) ? $data : [];
     }
-
-    /* private function getSuggestedAddress($words = [], $addressData)
-    {
-        if (!empty($words)) {
-            $wordsToSort = ['street', 'province', 'municipality', 'typeRoad'];
-
-            foreach ($wordsToSort as $frist) {
-                foreach ($wordsToSort as $second) {
-                    foreach ($wordsToSort as $third) {
-                        foreach ($wordsToSort as $fourth) {
-                            if ($frist !== $second && $frist !== $third && $frist !== $fourth && $second !== $third && $second !== $fourth && $third !== $fourth) {
-                                $wordsOrder[] = [$frist => null, $second => null, $third => null, $fourth => null];
-                            }
-                        }
-                    }
-                }
-            }
-            echo $wordsOrder; 
-
-            $maxWords = $this->getMaxWordsNumber($addressData, count($words));
-             for ($wordsStreet = $maxWords['street']; $wordsStreet >= 0; $wordsStreet--) {
-                for ($wordsProvince = $maxWords['province']; $wordsProvince >= 0; $wordsProvince--) {
-                    for ($wordsMunicipality = $maxWords['municipality']; $wordsMunicipality >= 0; $wordsMunicipality--) {
-                        for ($wordsTypeRoad = $maxWords['typeRoad']; $wordsTypeRoad  >= 0; $wordsTypeRoad--) {
-                            $sum = $wordsStreet + $wordsProvince + $wordsMunicipality + $wordsTypeRoad;
-                            if (count($words) === $sum) {
-                                $wordsLenght[] = ['street' => $wordsStreet, 'province' => $wordsProvince, 'municipality' => $wordsMunicipality, 'typeRoad' => $wordsTypeRoad];
-                            }
-                        }
-                    }
-                }
-            }
-
-            foreach ($wordsOrder as $orderArray) {
-                foreach ($wordsLenght as $lenghtArray) {
-                    $aux = $orderArray;
-                    foreach ($aux as $key => $valueOrder) {
-                        $valueOrder = $lenghtArray[$key];
-                        //si no funciona cambiar linea arriba
-                    }
-                    $combinations[] = $aux;
-                }
-            }
-
-            foreach ($combinations as $combination) {
-                $index = 0;
-                $aux = $combination;
-                $address = [];
-                foreach ($aux as $key => $value) {
-                    $text = '';
-                    for ($index; $index < $value + $index; $index++) {
-                        $text += $words[$index];
-                        $text += $index < ($value + $index - 1) ? ' ' : '';
-                    }
-                    $value = $text;
-                    $address[] = $aux;
-                }
-                foreach ($address as $key => $value) {
-                    if (str_contains($addressData['PROVINCIA'], $value['province']) && str_contains($addressData['MUNICIPIO'], $value['municipality']) && str_contains($addressData['TIPOVIA'], $value['typeRoad']) && str_contains($addressData['CALLE'], $value['street'])) {
-                        $data[] = [$addressData['TIPOVIA'], $addressData['CALLE'], $addressData['MUNICIPIO'], $addressData['PROVINCIA']];
-                    }
-                }
-            }
-        } else {
-            $data = [];
-        }
-        return $data;
-    } */
 
     /* private function executeNativeQuery($text, $params = [])
     {
