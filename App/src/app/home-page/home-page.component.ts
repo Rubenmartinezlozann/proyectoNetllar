d685b8ec4244c2abdaa6c857fee2a4a1ff55149d
ruben <rubenmartinezlozano@gmail.com>
2021-03-26 12:34:10 +0100

    Cambios en diseño de autocompletado de direcciones

--- App/src/app/home-page/home-page.component.ts
+++ App/src/app/home-page/home-page.component.ts
@@ -9,44 +9,72 @@ import { Router } from '@angular/router';
 })
 export class HomePageComponent implements OnInit {
 	data: any = [];
+	selectedData: any;
 	countRequest: number = 0;
 	number: number = 0
 	txtAddress: any;
+	products: any = [];
 
 	constructor(private http: HttpClient, private router: Router) { }
 
-	ngOnInit = () => { this.txtAddress = document.getElementById('txtAddress'); }
+	ngOnInit = () => {
+		this.txtAddress = document.getElementById('txtAddress');
+		console.log("ng on init ok");
+	}
 
-	getAddressText = (value: any) => value.currentTarget.value
+	getAddressText = (value: any) => value.currentTarget.value;
 
-	getNumberText = (value: any) => this.number = value.currentTarget.value
+	getNumberText = (value: any) => this.number = value.currentTarget.value;
 
 	getAddressData = (text: any) => {
 		this.countRequest++;
 		const num = this.countRequest;
 		const addressText = this.getAddressText(text);
-		if (addressText !== '') {
-			this.http.get(`http://127.0.0.1:8000/suggestAddress/${addressText}`).subscribe((res: any = []) => {
-				if (this.countRequest === num) this.data = res;
-			});
-		}
+		setTimeout(() => {
+			if (this.countRequest === num) {
+				if (addressText !== '') {
+					this.http.get(`http://127.0.0.1:8000/suggestAddress/${addressText}`).subscribe((res: any = []) => {
+						this.data = res;
+						if (this.selectedData !== undefined) this.selectedData = undefined;
+					});
+				}
+			}
+		}, 750);
 	}
 
 	writeAddress = (value: any) => {
 		this.txtAddress = document.getElementById('txtAddress');
 		this.txtAddress.value = `${value.typeRoad} ${value.street} ${value.township} ${value.province}`;
+		this.selectedData = {
+			'typeRoad': value.typeRoad,
+			'street': value.street,
+			'township': value.township,
+			'province': value.province,
+		};
+		this.data = [];
 	}
 
 	findProducts = () => {
-		console.log(this.data.length)
-		if (this.data.length === 1 && this.number > 0) {
-			this.http.get(`http://127.0.0.1:8000/findProducts/'${this.data[0].typeRoad}/${this.data[0].street}/${this.data[0].township}/${this.data[0].province}/${this.number}`)
-				.subscribe((res: any = null) => {
-					console.log("dentro");
-					if (res !== null) console.log(res); else console.log('mal');
+		if ((this.data.length <= 1 || this.selectedData !== undefined) && this.number > 0) {
+			let typeRoad: any;
+			let street: any;
+			let township: any;
+			let province: any;
+			if (this.selectedData !== undefined) {
+				typeRoad = this.selectedData.typeRoad;
+				street = this.selectedData.street;
+				township = this.selectedData.township;
+				province = this.selectedData.province;
+			} else {
+				typeRoad = this.data[0].typeRoad;
+				street = this.data[0].street;
+				township = this.data[0].township;
+				province = this.data[0].province;
+			}
+			this.http.get(`http://127.0.0.1:8000/findProducts/${typeRoad}/${street}/${township}/${province}/${this.number}`)
+				.subscribe((res: any) => {
+					this.products = res;
 				});
-		} else {
-			console.log("fuera");
 		}
 	}
 }
